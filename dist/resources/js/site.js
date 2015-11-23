506e27026aef7221e4b82fb3a7447f47643b14f1
Dong Huynh <don@upgroid.com>
2015-11-23 13:31:25 -0800

    cookie checks if visited before to disable newsletter popup.

--- dist/resources/js/site.js
+++ dist/resources/js/site.js
@@ -1029,14 +1029,30 @@ var Homepage =
     },
 
     "Newsletter": function () {
-        $(document).ready(function () {
-            console.log('firing');
-            var inst = $('[data-remodal-id=newsletter-modal]').remodal();
+        function setCookie(cname, cvalue, exdays) {
+            var d = new Date();
+            d.setTime(d.getTime() + (exdays*24*60*60*1000));
+            var expires = "expires="+d.toUTCString();
+            document.cookie = cname + "=" + cvalue + "; " + expires;
+        }
 
-            /**
-             * Opens the modal window
-             */
-            inst.open();
+        function getCookie(cname) {
+            var name = cname + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0; i<ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1);
+                if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+            }
+            return "";
+        }
+
+        $(document).ready(function () {
+            var inst = $('[data-remodal-id=newsletter-modal]').remodal();
+            if(!getCookie("username") == "visited") {
+                inst.open();
+                setCookie("username", "visited", 1);
+            }
         });
     },
 
