567f84c07b3c2b2d55f5aac7ad2cef101ea9d8de
don <don@updroid.com>
2015-12-10 13:54:39 -0800

    Exploded mobile view

--- src/resources/js/api.js
+++ src/resources/js/api.js
@@ -237,183 +237,194 @@ var Homepage =
         var controller = new ScrollMagic.Controller();
         var offset = ($("#exploded").height()/2);
 
-        new ScrollMagic.Scene({
-          triggerElement: "#exploded", 
-          offset: offset,
-          duration: 2700
-        })
-        .setPin('#exploded')
-        .addTo(controller);
+        var check = false;
+        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+        if(check == false) {
+            scrollScene();
+        }
+        
+        function scrollScene () {
 
-        new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 540,
-            duration: 200
-        }).setTween("#arm", {x: "15%", y: "-30%"})
-        .addTo(controller);
+            new ScrollMagic.Scene({
+              triggerElement: "#exploded", 
+              offset: offset,
+              duration: 2700
+            })
+            .setPin('#exploded')
+            .addTo(controller);
 
-        // arm scene duration, highlight pagination button
+            new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 540,
+                duration: 200
+            }).setTween("#arm", {x: "15%", y: "-30%"})
+            .addTo(controller);
 
-        var armScene = new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 540,
-            duration: 500
-        }).addTo(controller);
+            // arm scene duration, highlight pagination button
 
-        armScene.on('enter', function(event) {
-            armCopy.addClass('active');
-        });
+            var armScene = new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 540,
+                duration: 500
+            }).addTo(controller);
 
-        armScene.on('leave', function(event) {
-            armCopy.removeClass('active');
-        });
+            armScene.on('enter', function(event) {
+                armCopy.addClass('active');
+            });
 
-
-        new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 950,
-            duration: 200
-        }).setTween("#arm", {x: "-=15%", y: "+=30%"})
-        .addTo(controller);
-
-        // Io scene duration, highlight pagination button
-
-        var ioScene = new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 1150,
-            duration: 500
-        }).addTo(controller);
-
-        ioScene.on('enter', function(event) {
-            ioCopy.addClass('active');
-        });
-
-        ioScene.on('leave', function(event) {
-            ioCopy.removeClass('active');
-        });
-
-        new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 1150,
-            duration: 200
-        }).setTween("#io", {y: "-5%", x: "-12%"})
-        .addTo(controller);
+            armScene.on('leave', function(event) {
+                armCopy.removeClass('active');
+            });
 
 
+            new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 950,
+                duration: 200
+            }).setTween("#arm", {x: "-=15%", y: "+=30%"})
+            .addTo(controller);
 
-        new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 1550,
-            duration: 200
-        }).setTween("#io", {y: "+=5%", x: "+=12%"})
-        .addTo(controller);
+            // Io scene duration, highlight pagination button
 
-        // CPU scene duration, highlight pagination button
+            var ioScene = new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 1150,
+                duration: 500
+            }).addTo(controller);
 
-        var cpuScene = new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 1750,
-            duration: 500
-        }).addTo(controller);
+            ioScene.on('enter', function(event) {
+                ioCopy.addClass('active');
+            });
 
-        cpuScene.on('enter', function(event) {
-            cpuCopy.addClass('active');
-        });
+            ioScene.on('leave', function(event) {
+                ioCopy.removeClass('active');
+            });
 
-        cpuScene.on('leave', function(event) {
-            cpuCopy.removeClass('active');
-        });
+            new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 1150,
+                duration: 200
+            }).setTween("#io", {y: "-5%", x: "-12%"})
+            .addTo(controller);
 
 
 
-        new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 1750,
-            duration: 200
-        }).setTween("#cpu", {x: "15%", y: "-30%"})
-        .addTo(controller);
+            new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 1550,
+                duration: 200
+            }).setTween("#io", {y: "+=5%", x: "+=12%"})
+            .addTo(controller);
 
-        new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 2150,
-            duration: 200
-        }).setTween("#cpu", {x: "-=15%", y: "+=30%"})
-        .addTo(controller);
+            // CPU scene duration, highlight pagination button
 
-        // Remove cpu to make animation simpler
+            var cpuScene = new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 1750,
+                duration: 500
+            }).addTo(controller);
 
-        new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 2350,
-            duration: 5
-        }).setTween("#cpu", {opacity: 0})
-        .addTo(controller);
+            cpuScene.on('enter', function(event) {
+                cpuCopy.addClass('active');
+            });
 
-
-        // Base scene start, highlight pagination button
-
-        var baseScene = new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 2350,
-            duration: 500
-        }).addTo(controller);
-
-        var baseStart;
-
-        baseScene.on('enter', function(event) {
-            baseCopy.addClass('active');
-            baseStart = $(window).scrollTop();
-        });
-
-        baseScene.on('leave', function(event) {
-            if($(window).scrollTop() <= baseStart) {
-                baseCopy.removeClass('active');
-            }
-        });
-
-        new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 2350,
-            duration: 200
-        }).setTween("#arm", {y: "-40%"})
-        .addTo(controller);
-
-        new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 2350,
-            duration: 200
-        }).setTween("#io", {y: "-40%"})
-        .addTo(controller);
-
-        new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 2350,
-            duration: 200
-        }).setTween("#base", {y: "-20%"})
-        .addTo(controller);
+            cpuScene.on('leave', function(event) {
+                cpuCopy.removeClass('active');
+            });
 
 
 
-        new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 2750,
-            duration: 200
-        }).setTween("#arm", {y: "+=40%"})
-        .addTo(controller);
+            new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 1750,
+                duration: 200
+            }).setTween("#cpu", {x: "15%", y: "-30%"})
+            .addTo(controller);
 
-        new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 2750,
-            duration: 200
-        }).setTween("#io", {y: "+=40%"})
-        .addTo(controller);
+            new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 2150,
+                duration: 200
+            }).setTween("#cpu", {x: "-=15%", y: "+=30%"})
+            .addTo(controller);
 
-        new ScrollMagic.Scene({
-            triggerElement: "#exploded",
-            offset: 2750,
-            duration: 200
-        }).setTween("#base", {y: "+=20%"})
-        .addTo(controller);
+            // Remove cpu to make animation simpler
+
+            new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 2350,
+                duration: 5
+            }).setTween("#cpu", {opacity: 0})
+            .addTo(controller);
+
+
+            // Base scene start, highlight pagination button
+
+            var baseScene = new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 2350,
+                duration: 500
+            }).addTo(controller);
+
+            var baseStart;
+
+            baseScene.on('enter', function(event) {
+                baseCopy.addClass('active');
+                baseStart = $(window).scrollTop();
+            });
+
+            baseScene.on('leave', function(event) {
+                if($(window).scrollTop() <= baseStart) {
+                    baseCopy.removeClass('active');
+                }
+            });
+
+            new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 2350,
+                duration: 200
+            }).setTween("#arm", {y: "-40%"})
+            .addTo(controller);
+
+            new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 2350,
+                duration: 200
+            }).setTween("#io", {y: "-40%"})
+            .addTo(controller);
+
+            new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 2350,
+                duration: 200
+            }).setTween("#base", {y: "-20%"})
+            .addTo(controller);
+
+
+
+            new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 2750,
+                duration: 200
+            }).setTween("#arm", {y: "+=40%"})
+            .addTo(controller);
+
+            new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 2750,
+                duration: 200
+            }).setTween("#io", {y: "+=40%"})
+            .addTo(controller);
+
+            new ScrollMagic.Scene({
+                triggerElement: "#exploded",
+                offset: 2750,
+                duration: 200
+            }).setTween("#base", {y: "+=20%"})
+            .addTo(controller);
+            
+        }
+
     },
 
     "DIYkit": function () {
